595cead6c1dde8c566d2059ecbb2308905af2dad
ChanningKuo <492689363@qq.com>
2017-04-12 11:46:31 +0800

    发现菜单同步时间缓存失败的bug，添加了菜单的粗略布局

--- RekTecCRM/Views/ApplicationViewController.swift
+++ RekTecCRM/Views/ApplicationViewController.swift
@@ -13,6 +13,10 @@ class ApplicationViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var banaImage: UIImageView!
     
+    private let metroMenuColCount: CGFloat = 4
+    private let metroMenuPadding = 25
+    private let metroMenuIconWidth = 32
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,14 +29,31 @@ class ApplicationViewController: UIViewController {
     
     func menusRender(top: CGFloat, menus: [[String: Any]]){
         scrollView.frame = CGRect.init(x: 0, y: top, width: self.view.bounds.width, height: self.view.bounds.height - 49)
-        let row = menus.count / 3 + 1
-        scrollView.contentSize = CGSize.init(width: 0, height: row * 100)
+//        scrollView.contentSize = CGSize.init(width: 0, height: row * 100)
         scrollView.backgroundColor = .gray
-        // TODO 菜单布局
-        for (index, item) in menus.enumerated() {
-            let label = UILabel.init(frame: CGRect.init(x: 0, y: index * 20, width: Int(self.view.bounds.width), height: 100))
-            label.text = item["MenuUrl"] as? String
-            scrollView.addSubview(label)
+        
+        var menuButtonHeight: CGFloat = 0.0
+        let menuButtonWidth = self.view.bounds.width / metroMenuColCount
+        var col: CGFloat = 0
+        var row: CGFloat = 0
+        
+        for menu in menus {
+            menuButtonHeight = CGFloat(metroMenuPadding * 2 + 10 + metroMenuIconWidth + 10)
+            let button = UIButton.init(type: .custom)
+            button.backgroundColor = .clear
+            button.clearsContextBeforeDrawing = false
+            button.autoresizingMask = UIViewAutoresizing(rawValue: UInt(1 << 3))
+            button.setTitle(menu["MenuName"] as? String, for: .normal)
+            button.setTitleColor(UIColor.init(red: 0, green: 0.478431, blue: 1, alpha: 1), for: .normal)
+            button.setBackgroundImage(ImageUtils.imageWithColor(color: UIColor.init(red: 248 / 255, green: 248 / 255, blue: 248 / 255, alpha: 1), rect: CGRect.init(x: 0, y: 0, width: menuButtonWidth, height: menuButtonHeight)), for: .highlighted)
+            button.frame = CGRect.init(x: col * menuButtonWidth, y: row * menuButtonHeight, width: menuButtonWidth, height: menuButtonHeight)
+            scrollView.addSubview(button)
+            
+            col += 1
+            if col == metroMenuColCount {
+                col = 0
+                row += 1
+            }
         }
     }
 
@@ -40,7 +61,16 @@ class ApplicationViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
 
