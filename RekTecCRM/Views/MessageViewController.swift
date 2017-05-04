f0f4f77af5d09609e253af9cdf03fbfdd360bb0e
ChanningKuo <492689363@qq.com>
2017-05-04 11:35:41 +0800

    ready to design profile view

--- RekTecCRM/Views/MessageViewController.swift
+++ RekTecCRM/Views/MessageViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 
 class MessageViewController: UIViewController {
 
+    @IBOutlet weak var debugModel: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        debugModel.isOn = GlobalAppSetting.isHTML5Debug
+        
         print("This is First View")
     }
 
+    @IBAction func debugModelChange(_ sender: UISwitch) {
+        GlobalAppSetting.isHTML5Debug = !GlobalAppSetting.isHTML5Debug
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
